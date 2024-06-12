826dc290c77f2d97e6aa7c7fb1b9e9a7c8d74d6c
Бегалиев Нариман <begaliev.nariman@gmail.com>
2024-06-12 13:38:43 +0500

    Дописал функцию по хэшированию

--- back/src/middlewares/hash.ts
+++ back/src/middlewares/hash.ts
@@ -1,6 +1,6 @@
 import bcrypt from "bcrypt";
 
-async function hash(data: string) {
+export async function hash(data: string) {
     const salt = await bcrypt.genSalt(10);
     data = await bcrypt.hash(data, salt);
     return data;
