39fc8717db991bedd99a9790b7bcafe8c0a02d16
Stanislav <mion.hot.lips@mail.ru>
2024-08-13 22:03:25 +0500

    удалил закоментированный код

--- back/src/database/seeds/main.seeder.ts
+++ back/src/database/seeds/main.seeder.ts
@@ -1,11 +1,25 @@
 import { DataSource } from 'typeorm';
 import { Seeder, SeederFactoryManager } from 'typeorm-extension';
 import { User } from '@/entities/user.entity';
-
+import bcrypt from 'bcrypt';
 export default class MainSeeder implements Seeder {
     public async run(dataSource: DataSource, factoryManager: SeederFactoryManager): Promise<void> {
         const userFactory = factoryManager.get(User);
 
-        await userFactory.saveMany(5)
+        // Создаем статичного пользователя (админа)
+        const salt = await bcrypt.genSalt(10);
+        const admin = new User();
+        admin.username = 'admin';
+        admin.email = 'admin@example.com';
+        admin.generateToken();
+        admin.isEmailConfirmed = true;
+        admin.role = 'admin';
+        admin.password = await bcrypt.hash('admin123', salt);
+
+        await dataSource.getRepository(User).save(admin);
+
+        // Создаем и сохраняем пять случайных пользователей
+        const users = await userFactory.saveMany(5);
+        await dataSource.getRepository(User).save(users);
     };
 };
--- back/src/routes/card.route.ts
+++ back/src/routes/card.route.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 import { IRoute } from '@/interfaces/IRoute.interface';
 import { CardController } from '@/controllers/card.controller';
 import { upload } from '@/middlewares/upload';
+import { checkRole } from '@/middlewares/checkRole';
+
 
 export class CardRoute implements IRoute {
   public path = '/cards';
@@ -18,16 +20,16 @@ export class CardRoute implements IRoute {
     this.router.get('/all', this.controller.getAllCards);
     this.router.get('/show', this.controller.getShowCards);
     this.router.get('/:id', this.controller.getCard);
-    // this.router.post('/', upload.single('image'), upload.single('video'), this.controller.createCard);
     this.router.post(
       '/',
       upload.fields([
         { name: 'image', maxCount: 1 },
         { name: 'video', maxCount: 1 }
-      ]),
+      ]),checkRole('admin'),
+      
       this.controller.createCard
     );
-    this.router.delete('/delete/:id', this.controller.deleteCard);
+    this.router.delete('/delete/:id',checkRole('admin'),this.controller.deleteCard);
     this.router.put('/update/:id', this.controller.updateCard);
   }
 }
