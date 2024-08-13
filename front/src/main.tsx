07a2421cd8fa2346fc47f15cf5e3f746fd31e8ba
Жанабаев Алишер <alyzhan04@gmail.com>
2024-08-13 23:38:31 +0500

    Добавил иконки, обновил package

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -6,13 +6,29 @@ import i18n from "@/i18n.ts";
 import store, { persistor } from "./app/store.ts";
 import App from "./containers/App.tsx";
 import "./index.css";
+import { ThemeProvider, createTheme } from '@mui/material/styles'; 
+
+const theme = createTheme({
+  breakpoints: {
+      values: {
+          xs: 0,
+          sm: 600,
+          md: 960,
+          lg: 1366,  
+          xl: 1920, 
+      },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <I18nextProvider i18n={i18n}>
-        <App />
+        <ThemeProvider theme={theme}> 
+          <App />
+        </ThemeProvider>
       </I18nextProvider>
     </PersistGate>
   </Provider>
 );
+
