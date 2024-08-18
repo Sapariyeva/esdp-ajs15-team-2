67931f0f46a38f518752a1da451efef8fe7bb728
Rustam <ruskhass@gmail.com>
2024-08-18 12:04:52 +0500

    chore: change passportConfig.ts

--- back/src/config/passportConfig.ts
+++ back/src/config/passportConfig.ts
@@ -2,14 +2,24 @@ import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import { UserRepository } from '../repositories/user.repository';
 import { User } from '../entities/user.entity';
+import path from 'node:path';
+import fs from 'node:fs';
 
-const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID!;
+const GOOGLE_CLIENT_ID = () => {
+    if (!process.env.GOOGLE_CLIENT_ID) {
+        const data = fs.readFileSync(path.join(__dirname, './.env'));
+        console.log(data);
+        
+        throw new Error('No GOOGLE_CLIENT_ID environment variable');
+    }
+    return process.env.GOOGLE_CLIENT_ID!
+};
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET!;
 
 passport.use(new GoogleStrategy({
-    clientID: GOOGLE_CLIENT_ID,
+    clientID: GOOGLE_CLIENT_ID(),
     clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:8000/auth/google/callback' // TODO: host
+    callbackURL: 'http://localhost:8000/auth/google/callback' // TODO: host (servers IP)
 }, async (accessToken, refreshToken, profile, done) => {
     if (!profile.emails || profile.emails.length === 0) {
         return done(new Error('No email found in profile'), undefined);
