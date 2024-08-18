9e32d9acd6fa31fa244a0f0fb37defa808a71619
Rustam <ruskhass@gmail.com>
2024-08-18 17:06:31 +0500

    chore: try to fix locale

--- back/src/dataSource/dataSource.ts
+++ back/src/dataSource/dataSource.ts
@@ -3,6 +3,12 @@ import { SeederOptions } from "typeorm-extension";
 import MainSeeder from "../database/seeds/main.seeder";
 // import { User } from "../entities/user.entity";
 import { UserFactory } from "../database/factories/user.factory";
+import path from 'path';
+
+console.log('DIRNAME====', path.join(__dirname, `/*.js`));
+console.log('NODE_ENV====', process.env.NODE_ENV);
+console.log('PROCESS====', process.env.npm_lifecycle_script?.startsWith('cross-env NODE_ENV=development'));
+
 
 /**
  * Данная конфигурация нужна при запуске команды npm run dev
@@ -10,13 +16,13 @@ import { UserFactory } from "../database/factories/user.factory";
 const options: DataSourceOptions & SeederOptions = {
     type: 'mysql',
     host: 'localhost',
-    port: 3306,
-    username: 'igrovuz',
-    password: "Igrovuz2024~!",
+    port: process.env.npm_lifecycle_script?.startsWith('cross-env NODE_ENV=development')? 3307 : 3306,
+    username: process.env.npm_lifecycle_script?.startsWith('cross-env NODE_ENV=development') ? 'root' : 'igrovuz',
+    password: process.env.npm_lifecycle_script?.startsWith('cross-env NODE_ENV=development') ? "" : 'Igrovuz2024~!',
     database: 'esdp',
     synchronize: true,
     logging: true,
-    entities: [`dist/entities/**/*.js`],
+    entities: [process.env.npm_lifecycle_script?.startsWith('cross-env NODE_ENV=development') ? `src/entities/*{.ts,.js}` : path.join(__dirname, `/*.js`)],
     seeds: [MainSeeder],
     factories: [UserFactory]
 }
