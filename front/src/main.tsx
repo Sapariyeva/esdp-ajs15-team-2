1636fd1426d88b9edd2fd75405131cf6f8253b3f
Rustam <ruskhass@gmail.com>
2024-08-18 17:16:59 +0500

    chore: вернул импорт i18n

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { I18nextProvider } from "react-i18next";
-import i18n from "i18n.ts";
+import i18n from "@/i18n.ts";
 import store, { persistor } from "./app/store.ts";
 import App from "./containers/App.tsx";
 import "./index.css";
