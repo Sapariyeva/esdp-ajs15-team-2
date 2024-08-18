03b1853125e07579c1c56eed1eabf14ff722b857
Rustam <ruskhass@gmail.com>
2024-08-18 12:33:43 +0500

    chore: remove aliases authValidate

--- back/src/middlewares/authValidate.ts
+++ back/src/middlewares/authValidate.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
-import { IRequestWithUser } from "@/interfaces/IRequestWithUser.interface";
-import { IUser } from "@/interfaces/IUser.interface";
-import { UserService } from "@/services/user.service";
+import { IRequestWithUser } from "../interfaces/IRequestWithUser.interface";
+import { IUser } from "../interfaces/IUser.interface";
+import { UserService } from "../services/user.service";
 
 export const authValidate = async (req: Request, res: Response, next: NextFunction) => {
   const token = req.header("Authorization");
